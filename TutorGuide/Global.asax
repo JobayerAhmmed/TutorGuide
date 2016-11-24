714eff2c144c4de2c9e1bedb0a7571b7ae55a312
JobayerAhmmed <jobayer.ahmmed@gmail.com>
2016-11-24 14:31:53 +0600

    Add project files.

---
+++ TutorGuide/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TutorGuide.MvcApplication" Language="C#" %>
---
+++ TutorGuide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TutorGuide.Startup))]
+namespace TutorGuide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
